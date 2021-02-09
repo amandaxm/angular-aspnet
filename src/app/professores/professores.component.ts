dbbcccddb7da575a76ad3645b0a8e79154aa0c19
Marcelo Borba <marceloborba@frwk.com.br>
2021-02-09 12:08:22 -0300

    Mesmo para professor

--- src/app/professores/professores.component.ts
+++ src/app/professores/professores.component.ts
@@ -6,13 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./professores.component.scss']
 })
 export class ProfessoresComponent implements OnInit {
-  titulo= 'Professores'  
+  titulo= 'Professores';
+  professorSelecionado: any;  
   professores=[
     {id:1, nome:'Lauro', sobrenome:'Paulo', disciplina:'Matematica'},
     {id:2, nome:'Caio', sobrenome:'Carlo', disciplina:'Geografia'},
     {id:3, nome:'Carla', sobrenome:'Rodrigues', disciplina:'Portugues'},
     {id:4, nome:'Leticia', sobrenome:'Teste', disciplina:'Fisica'},
      ]
+     professorSelect(professor: any){
+      this.professorSelecionado = professor.nome;
+    }
+  
+    v(){
+      this.professorSelecionado = '';
+    }
   constructor() { }
 
   ngOnInit() {
